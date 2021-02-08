517205f9eff87f6c5676e90584e9f7de812d7fdc
Ellen Hoeven <ellen.hoeven@ibm.com>
2021-02-08 22:37:37 +0100

    Add modeling utils functions Create targets Create features Create train test split

---
+++ src/utils/modeling_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+"""
+Modeling_utils.py: our own functions that are needed in various modeling scripts
+- create features and targets
+- create train_test_splits
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def create_target(train_data: pd.DataFrame) -> pd.DataFrame:
+    y = train_data['diabetes_mellitus']
+    return y
+
+
+def create_features(train_data: pd.DataFrame) -> pd.DataFrame:
+    X = train_data.drop(['diabetes_mellitus'], axis=1)
+    return X
+
+
+def create_train_test_split(features: pd.DataFrame, target: pd.DataFrame,
+                            train_perc: float, test_perc: float):
+
+    X_train, X_test, y_train, y_test = train_test_split(features,
+                                                        target,
+                                                        train_size=train_perc,
+                                                        test_size=test_perc,
+                                                        random_state=42,
+                                                        stratify=True)
+    return X_train, X_test, y_train, y_test
