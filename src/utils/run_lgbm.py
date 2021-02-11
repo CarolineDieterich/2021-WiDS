23c7b016b434238de6fd52da607542403da7e798
Ellen Hoeven <ellen.hoeven@ibm.com>
2021-02-11 22:27:49 +0100

    Finalize first working version of lgbm model Save and export predictions Train on full data set Predict on unlabeled data set

--- src/utils/run_lgbm.py
+++ src/utils/run_lgbm.py
@@ -10,16 +10,19 @@ import logging
 import lightgbm as lgbm
 import pandas as pd
 import numpy as np
-import typing as th
 from sklearn.metrics import roc_auc_score
 from pathlib import Path
 
-from modeling_utils import create_target, create_features, create_train_test_split # , save_and_export_predictions
+from modeling_utils import create_target, create_features, create_train_test_split, save_and_export_predictions
 
 
 class LgbmModel:
 
-    def __init__(self, input_path=None):
+    def __init__(self, input_path=None, output_path=None, raw_path=None):
+        self.input_path = input_path
+        self.output_path = output_path
+        self.raw_path = raw_path
+
         self.train_data = pd.read_csv(str(input_path) + '/train_data.csv', sep=';')
         self.test_data = pd.read_csv(str(input_path) + '/test_data.csv', sep=';')
 
@@ -35,6 +38,7 @@ class LgbmModel:
                                                                                        0.3)
         self.d_train = lgbm.Dataset(self.X_train, self.y_train, free_raw_data=False)
         self.d_test = lgbm.Dataset(self.X_test, self.y_test, free_raw_data=False)
+        self.d_train_full = lgbm.Dataset(self.X, self.y, free_raw_data=False)
 
     @staticmethod
     # TODO parameter tuning https://lightgbm.readthedocs.io/en/latest/Parameters-Tuning.html
@@ -109,7 +113,7 @@ class LgbmModel:
 
         return lgbm_model
 
-    def make_preds(self, lgbm_model) -> np.array:
+    def make_preds_test(self, lgbm_model) -> np.array:
         """
 
         Args:
@@ -119,27 +123,76 @@ class LgbmModel:
 
         """
         logging.info("Calculating lgbm predictions...")
-
         predictions_test = lgbm_model.predict(self.X_test)
         print(roc_auc_score(self.y_test, predictions_test))
-
         return predictions_test
 
-    def make_and_save_final_preds(self):
-        pass
+    @staticmethod
+    def get_most_important_features(lgbm_model) -> np.array:
+        """
+
+        Args:
+            lgbm_model:
+
+        Returns:
+
+        """
+        logging.info("Retrieving most important features for lgbm model...")
+        feature_importance_values = lgbm_model.feature_importance()
+        feature_names = lgbm_model.feature_name()
+        feature_importances = pd.DataFrame({'feature': feature_names, 'importance': feature_importance_values})
+        # Sort features according to importance
+        feature_importances = feature_importances.sort_values('importance', ascending=False).reset_index(drop=True)
+        print(feature_importances[0:30])
+
+    def train_final_model(self, lgbm_model, params):
+        """
+
+        Args:
+            lgbm_model:
+            params:
+
+        Returns:
+
+        """
+        logging.info("Final training with hyperparameters on full dataset...")
+        # training without early stop
+        lgbm_trainall = lgbm.train(params,
+                                    self.d_train_full,
+                                    lgbm_model.current_iteration(),
+                                    verbose_eval=100)
+        return lgbm_trainall
+
+    def make_and_save_final_preds(self, lgbm_trainall, file_name):
+        """
+
+        Args:
+            lgbm_trainall:
+            file_name:
+
+        Returns:
+
+        """
+        preds_unlabeled_lgbm = lgbm_trainall.predict(self.test_data)
+        save_and_export_predictions(preds_unlabeled_lgbm, file_name, self.raw_path, self.output_path)
+        return preds_unlabeled_lgbm
 
     def run(self):
         params = self.set_params()
         lgbm_cv_hist = self.train_lgbm_cv_with_early_stop(params)
         lgbm_model = self.train_lgbm(params, lgbm_cv_hist)
-        predictions = self.make_preds(lgbm_model)
-        # save_and_export_predictions(predictions, self.X_test)
-        # FIXME save predictions and importances
+        self.get_most_important_features(lgbm_model)
+        lgbm_trainall = self.train_final_model(lgbm_model, params)
+        predictions_test_set = self.make_preds_test(lgbm_model)
+        predictions_val_set = self.make_and_save_final_preds(lgbm_trainall, 'lgbm_preds')
+        print(predictions_val_set)
 
 
 if __name__ == "__main__":
     base_path = str(Path(__file__).resolve().parents[2])
 
-    lgbm_predictor = LgbmModel(input_path=base_path + '/data/prepared_data')
+    lgbm_predictor = LgbmModel(input_path=base_path + '/data/prepared_data',
+                               output_path=base_path + '/data/submission',
+                               raw_path=base_path + '/data/raw')
     lgbm_predictor.run()
 
