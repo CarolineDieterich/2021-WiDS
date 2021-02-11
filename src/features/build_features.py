a866a539fce930d71584dc36895507db9a07e0a7
Ellen Hoeven <ellen.hoeven@ibm.com>
2021-02-11 23:16:13 +0100

    FE: add imputed, scaled and combined features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -12,6 +12,9 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import QuantileTransformer
+
 
 class DataPreparation:
 
@@ -48,9 +51,10 @@ class DataPreparation:
         self.train_data = self.train_data[cols_to_keep]
         self.test_data = self.test_data[cols_to_keep_test]
 
+    # FIXME ensure that this is only done for numerical cols
     def remove_cols_colinear(self, correlation_thresh) -> None:
         """Remove highly colinear columns"""
-        corr_matrix = self.train_data.corr().abs()
+        corr_matrix = self.train_data.corr(method='pearson').abs()
         # Select upper triangle of correlation matrix
         upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(np.bool))
         # Find features with correlation greater than thresh
@@ -70,36 +74,87 @@ class DataPreparation:
         first_hour_measurement_present_rows = self.train_data.loc[self.train_data[first_hour_cols].isnull().sum(axis=1)
                                                                   /len(first_hour_cols)!=1]
 
-    # FIXME add further functions for feature engineering here
-    def further_feature_engineering(self):
-        pass
+    def impute_features(self):
+
+        featu_int = []
+        featu_float = []
+        featu_obj = []
+
+        for col in self.test_data.columns:
+            x = self.test_data[col].dtype
+            if x == 'int64' or x == 'int32':
+                # self.train_data[col] = self.train_data[col].astype('int32')
+                featu_int.append(col)
+            elif x == 'float64' or x == 'float32':
+                # table_train[col] = table_train[col].astype('float32')
+                featu_float.append(col)
+            else:
+                featu_obj.append(col)
+
+        imp_simp = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+        imputer_int = SimpleImputer(missing_values=np.nan,
+                                    strategy='mean')  # KNNImputer(n_neighbors=2, weights="uniform")
+        imputer_float = SimpleImputer(missing_values=np.nan,
+                                      strategy='mean')  # KNNImputer(n_neighbors=2, weights="uniform")
+
+        # Train
+        self.train_data[featu_float] = imputer_float.fit_transform(self.train_data[featu_float])
+        self.train_data[featu_int] = imputer_int.fit_transform(self.train_data[featu_int]).astype(int)
+        self.train_data[featu_obj] = imp_simp.fit_transform(self.train_data[featu_obj])
+        # Test
+        self.test_data[featu_float] = imputer_float.transform(self.test_data[featu_float])
+        self.test_data[featu_int] = imputer_int.transform(self.test_data[featu_int]).astype(int)
+        self.test_data[featu_obj] = imp_simp.transform(self.test_data[featu_obj])
+
+        return featu_int, featu_float, featu_obj
+
+    def scale_features(self, featu_float) -> None:
+        qt = QuantileTransformer(n_quantiles=100, random_state=0, output_distribution="normal")
+
+        self.train_data[featu_float] = qt.fit_transform(self.train_data[featu_float])
+        self.test_data[featu_float] = qt.transform(self.test_data[featu_float])
+
+    def create_shuffled_features(self, data):
+        def create_features(x):
+            # g_e = x['ethnicity'] + '_' + x['gender']
+            a_bmi = x['age'] * x['bmi']
+            a_glu = x['age'] * x['d1_glucose_max']
+            bmi_glu = x['bmi'] * x['d1_glucose_max']
+            return pd.Series([a_bmi, a_glu, bmi_glu], index=['age_bmi', 'a_glu', 'bmi_glu'])
+        return data.join(data.apply(create_features, axis=1))
 
     def save_preprocessed_files(self):
         self.train_data.to_csv(Path(self.output_dir + '/train_data.csv'), sep=';')
         self.test_data.to_csv(Path(self.output_dir + '/test_data.csv'), sep=';')
 
+    # FIXME check order
     def run(self):
         self.drop_unnecessary_cols(['Unnamed: 0',
                                     'encounter_id',
                                     'hospital_id',
                                     'icu_id',
-                                    'icu_stay_type',
                                      'readmission_status'])
+        self.remove_invalid_rows()
+        # self.add_flag_measurements_first_hour()
+        self.remove_cols_with_nans(missing_perc=0.75)
+        self.remove_cols_colinear(correlation_thresh=0.95)
+        self.train_data = self.create_shuffled_features(self.train_data)
+        self.test_data = self.create_shuffled_features(self.test_data)
         self.create_dummy_cols(['elective_surgery',
                                 'ethnicity',
+                                # 'ethnicity_gender',
                                 'gender',
                                 'hospital_admit_source',
                                 'icu_admit_source',
-                                'icu_type'])
+                                'icu_type',
+                                'icu_stay_type',])
         self.drop_cols_not_in_test(['hospital_admit_source_ICU',
                                     'hospital_admit_source_Other',
                                     'hospital_admit_source_PACU',
                                     'hospital_admit_source_Observation',
                                     'hospital_admit_source_Acute Care/Floor'])
-        self.remove_invalid_rows()
-        # self.add_flag_measurements_first_hour()
-        self.remove_cols_with_nans(missing_perc=0.8)
-        self.remove_cols_colinear(correlation_thresh=0.95)
+        featu_int, featu_float, featu_obj = self.impute_features()
+        self.scale_features(featu_float)
         self.save_preprocessed_files()
 
 
